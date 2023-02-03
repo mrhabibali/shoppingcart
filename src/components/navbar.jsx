40b7739a63a722373a004ccbca98a21450dc96fc
mrhabibali <habibali92@hotmail.com>
2023-02-03 20:55:10 +0100

    testing a change

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Navbar is working
           <span className="badge pill bg-primary">
             {this.props.totalCounters}
           </span>
